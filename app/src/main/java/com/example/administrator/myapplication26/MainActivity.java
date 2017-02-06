3cc23f712052a0495217e93f40d586e9bff124bf
wangjinqiao <898179404@qq.com>
2017-02-06 16:46:47 +0800

    butterknife

--- app/src/main/java/com/example/administrator/myapplication26/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication26/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.administrator.myapplication26;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txt)
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        txt.setText("######");
+    }
+
+    @OnClick(R.id.txt)
+    public void onClick() {
     }
 }
